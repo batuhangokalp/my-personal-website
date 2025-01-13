3e572f389f3c178aee9c6e2671f32755b671a88a
Batuhan Gökalp <94108168+batuhangokalp@users.noreply.github.com>
2025-01-13 16:37:42 +0300

    Update skills-card.component.jsx

--- src/components/skills-card/skills-card.component.jsx
+++ src/components/skills-card/skills-card.component.jsx
@@ -21,10 +21,10 @@ const SkillsCard = () => {
 			<SectionTitle>Technology Stack</SectionTitle>
 			<StackList>
 				<Techs>
-					I'm currently focused on<Tech style={{ borderBottom: '3px solid #0db7ed' }}>React Native</Tech>,<Tech style={{ borderBottom: '3px solid #ff9900' }}>React</Tech>and<Tech style={{ borderBottom: '3px solid rgb(0, 0, 128' }}>Next.js</Tech>.
+					I'm currently focused on<Tech style={{ borderBottom: '3px solid #0db7ed' }}>React</Tech>,<Tech style={{ borderBottom: '3px solid #ff9900' }}>React Native</Tech>,<Tech style={{ borderBottom: '3px solid #16a182' }}>Express.js</Tech>and<Tech style={{ borderBottom: '3px solid rgb(0, 0, 128' }}>MongoDB</Tech>.
 				</Techs>
 				<Techs>
-					I've knowledge on <Tech style={{ borderBottom: '3px solid #cf4f4f' }}>PostgreSQL</Tech>, <Tech style={{ borderBottom: '3px solid rgb(0, 128, 128' }}>Postman</Tech>, <Tech style={{ borderBottom: '3px solid rgb(128, 0, 0)' }}>Express.js</Tech>, <Tech style={{ borderBottom: '3px solid #0db7ed' }}>Docker</Tech>.
+					I've knowledge on <Tech style={{ borderBottom: '3px solid #cf4f4f' }}>PostgreSQL</Tech>, <Tech style={{ borderBottom: '3px solid rgb(0, 128, 128' }}>Postman</Tech>, <Tech style={{ borderBottom: '3px solid rgb(128, 0, 0)' }}>Next.js</Tech>, <Tech style={{ borderBottom: '3px solid #0db7ed' }}>Docker</Tech>.
 				</Techs>
 				<Techs>
 					He's my best friend now<Tech style={{ borderBottom: '3px solid #16a182' }}>ChatGPT</Tech>.
